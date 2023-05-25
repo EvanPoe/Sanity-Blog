0d2aae2eb96296a9a9cc5cd550f08c4a17195266
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-05-25 16:57:48 -0500

    basic blog schema

--- sanity-blog-studio/schemas/index.js
+++ sanity-blog-studio/schemas/index.js
@@ -1,1 +1,24 @@
-export const schemaTypes = []
+export const schemaTypes = [
+  {
+    name: 'blog',
+    type: 'document',
+    title: 'Blog',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title',
+      },
+      {
+        name: 'subtitle',
+        type: 'string',
+        title: 'Subtitle',
+      },
+      {
+        name: 'slug',
+        type: 'slug',
+        title: 'Slug',
+      },
+    ],
+  },
+]
