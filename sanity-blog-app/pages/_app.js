1ef4d061bf3b7b954bb3374aa22e2363d3c3e665
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-05-25 11:11:42 -0500

    react bootstrap integration and rework to default next folder structure

---
+++ sanity-blog-app/pages/_app.js
@@ -0,0 +1,6 @@
+import { Component } from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// entry point to application in Next.js
+export default ({ Component, pageProps }) => <Component {...pageProps} />;
