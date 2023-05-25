46246e0e4a04271ba61b067bed6bced8428f297b
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-05-25 16:13:12 -0500

    component separation

---
+++ sanity-blog-app/components/PageLayout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BlogNavbar } from "./BlogNavbar";
+
+export const PageLayout = ({ children, className }) => {
+  return (
+    <Container>
+      <BlogNavbar />
+      <div className={`page-wrapper ${className}`}>{children}</div>
+      <footer className="page-footer">
+        <div>
+          <a href="#">courses</a>
+          {" | "}
+          <a href="#">github</a>
+          {" | "}
+          <a href="#">facebook</a>
+        </div>
+      </footer>
+    </Container>
+  );
+};
--- sanity-blog-app/pages/_app.js
+++ sanity-blog-app/pages/_app.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/index.scss';
+import '@/styles/index.scss';
 
 // entry point to application in Next.js
 export default ({ Component, pageProps }) => <Component {...pageProps} />;
