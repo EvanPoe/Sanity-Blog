7c1cb2cbb06fc51d713af29735ab78b40bd833ab
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-06-01 11:30:16 -0500

    get static paths set up for blog pages

--- sanity-blog-app/pages/blogs/[slug].js
+++ sanity-blog-app/pages/blogs/[slug].js
@@ -1,13 +1,31 @@
 import { PageLayout } from "@/components/PageLayout";
-import { useRouter } from "next/router";
 
-const BlogDetail = () => {
-  const { query } = useRouter();
+import { getBlogBySlug, getAllBlogs } from "lib/api";
+
+const BlogDetail = ({ blog }) => {
   return (
     <PageLayout>
-      <h1>Hello Detail Page - {query?.slug}</h1>
+      <h1>Hello Detail Page - {blog?.slug}</h1>
     </PageLayout>
   );
 };
+
+export async function getStaticProps({ params }) {
+  const blog = await getBlogBySlug(params.slug);
+  return {
+    props: { blog },
+  };
+}
+
+export async function getStaticPaths() {
+  const blogs = await getAllBlogs();
+  const paths = blogs?.map((blog) => ({
+    params: { slug: blog.slug },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default BlogDetail;
